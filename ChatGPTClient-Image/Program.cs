b63031712be9f993eacbf818fdead7e5d4257c42
Adams-Developer <tadams03@mail.roosevelt.edu>
2023-03-17 23:53:40 -0500

    Add nuget package

--- ChatGPTClient-Image/Program.cs
+++ ChatGPTClient-Image/Program.cs
@@ -1,4 +1,3 @@
-using ChatGPTClient_Image.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -13,7 +12,6 @@ namespace ChatGPTClient_Image
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
